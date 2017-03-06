7162c38afb0c665b111aafc3257468dab0601688
Andrew Frauens <andrew.frauens+GH@gmail.com>
2017-03-05 23:43:16 -0500

    Needs Testing, but might be finished

--- nnScript.py
+++ nnScript.py
@@ -3,6 +3,10 @@ from scipy.optimize import minimize
 from scipy.io import loadmat
 from math import sqrt
 
+import datetime as dt
+
+truth_matrix = np.zeros((50000, 10))
+
 
 def initializeWeights(n_in, n_out):
     epsilon = sqrt(6) / sqrt(n_in + n_out + 1)
@@ -12,10 +16,6 @@ def initializeWeights(n_in, n_out):
 
 def sigmoid(z):
     return  1 / (1 + np.exp(-z))
-    
-def my_preprocess() :
-    mat = loadmat('SmallFile.mat')
-    
 
 
 def preprocess():
@@ -110,24 +110,75 @@ def preprocess():
     test_data = test_data[:,bool_index == False]
     test_data = test_data / 255.0
     
+    
+    global truth_matrix
+
+    for i in range(10000):
+        truth_matrix[i,train_label[i]] = 1
+    
+
+
     print('preprocess done')
     return train_data, train_label, validation_data, validation_label, test_data, test_label 
 
-def ErrorFcn(w1, w2, o, training_label, n_class, n_input):
-    y = np.zeros(n_input, n_class);
-    for i in range(n_input):
-        y[i,training_label[i]] = 1
+def ErrorFcn(w1, w2, o, n_class, n_input):
+    global truth_matrix
+    y = truth_matrix
     
     A = np.log(o)
     B = np.log(1-o)
+
+    pre = (np.multiply(y,A))
+    post = (np.multiply((1-y),B))
     
-    error_sum =  -(1/n_input) * np.sum((np.multiply(y,A)) ,(np.multiply((1-y),B)))
+    error_sum =  -(1/n_input) * (np.sum( pre) + np.sum(post))
     return error_sum
     
-def reg_ErrorFcn(w1, w2, o, training_label, n_class, n_input, lamda):
-    no_reg = ErrorFcn(w1, w2, o, training_label, n_class, n_input)
-    reg_part = lamda/(2*n_input) * (np.square(w1).sum() + np.square(w2).sum())
+def reg_ErrorFcn(w1, w2, o, n_class, n_input, lamda):
+    no_reg = ErrorFcn(w1, w2, o, n_class, n_input)
+    reg_part = lamda/(2*10000) * (np.sum(np.square(w1)) + np.sum(np.square(w2)))
     return no_reg + reg_part
+    
+def gradFcn(w1, w2, z, o, n_input, n_hidden, n_class, lamda, data):
+    
+    global truth_matrix
+    y = truth_matrix
+
+    sigma = o-y
+
+    grad_w2 = np.zeros(( n_class, n_hidden+1, 10000))
+    for image in range(10000):
+        for h_node in range(n_hidden+1):
+            for o_node in range(n_class):
+                if (not(h_node == n_hidden)):
+                    grad_w2[ o_node, h_node, image] = sigma[image, o_node] * z[image, h_node]
+                else: 
+                    grad_w2[ o_node, h_node, image] = sigma[image, o_node] 
+    
+    reg_grad_w2 = grad_w2.sum(axis=2)
+
+    reg_grad_w2 = reg_grad_w2 + lamda*w2
+    reg_grad_w2 = reg_grad_w2/10000
+    
+    
+    grad_w1 = np.zeros((n_input+1, n_hidden, 10000))
+    for image in range(10000):
+        for h_node in range(n_hidden):
+            for pixel in range(n_input+1):
+                temp_sum = 0
+                for o_node in range(n_class):
+                    temp_sum += sigma[image, o_node]*w2[ o_node, h_node]
+                    
+                if not(pixel == n_input):
+                    grad_w1[pixel, h_node] = (1-z[image,h_node])*z[image,h_node]*data[image,pixel] * temp_sum
+                else:
+                    grad_w1[pixel, h_node] = (1-z[image,h_node])*z[image,h_node] * temp_sum
+                        
+    reg_grad_w1 = grad_w1.sum(axis=2)
+    reg_grad_w1 = reg_grad_w1 + lamda * w1
+    reg_grad_w2 = reg_grad_w2/10000
+    
+    return reg_grad_w1, reg_grad_w2
 
 def nnObjFunction(params, *args):
     """% nnObjFunction computes the value of objective function (negative log 
@@ -173,15 +224,23 @@ def nnObjFunction(params, *args):
     
 
     # Your code here
-    training_data_y, training_data_x = training_data.shape
-    in_data = np.c_[train_data, np.ones(training_data_y)]
+     
+    z, o, _ = nnFeedForward(w1,w2,training_data)
+    
+    
+    
+    obj_val = reg_ErrorFcn(w1, w2, o, n_class, n_input, lambdaval)
+   
+    grad_w1, grad_w2 = gradFcn(w1, w2, z, o, n_input, n_hidden, n_class, lambdaval, training_data)
 
 
     # Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
     # you would use code similar to the one below to create a flat array
-    # obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
-    obj_grad = np.array([0])
+    obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
 
+    global numIters
+    numIters+=1
+    print("The obj fn has been called {} times. (t:{})".format(numIters,dt.datetime.now()))
     return (obj_val, obj_grad)
 
 
@@ -209,46 +268,46 @@ def nnPredict(w1, w2, data):
 def nnFeedForward(w1,w2,data):
     w1 = np.transpose(w1)
     w2 = np.transpose(w2)
-    print ("This is the W1 matrix")
-    print (w1)
-    print (" ")
-    print ("This is the W2 matrix")
-    print (w2)
-    print (np.equal.reduce(w2))
-    print (" ")
-    print ("This is the data set :")
-    print (data)
-    print (np.equal.reduce(data))
-    print (" ")
+#    print ("This is the W1 matrix")
+#    print (w1)
+#    print (" ")
+#    print ("This is the W2 matrix")
+#    print (w2)
+#    print (np.equal.reduce(w2))
+#    print (" ")
+#    print ("This is the data set :")
+#    print (data)
+#    print (np.equal.reduce(data))
+#    print (" ")
     data_y, data_x = data.shape
-    
+#    
     in_data = np.c_[data, np.ones(data_y)]
-    print ("This is data after adding bias")
-    print (in_data)
-    print (np.equal.reduce(in_data , axis = 0))
-    print (" ")
+#    print ("This is data after adding bias")
+#    print (in_data)
+#    print (np.equal.reduce(in_data , axis = 0))
+#    print (" ")
     hidden_layer = np.dot(in_data, w1)
-    print ("This is the hidden_layer")
-    print (np.equal.reduce(hidden_layer))
-    print (" ")
+#    print ("This is the hidden_layer")
+#    print (np.equal.reduce(hidden_layer))
+#    print (" ")
     sig_hidden = sigmoid(hidden_layer)
-    print ("This is the sig_hidden")
-    print (sig_hidden)
-    print (" ")
+#    print ("This is the sig_hidden")
+#    print (sig_hidden)
+#    print (" ")
     sig_hidden_y, sig_hidden_x = hidden_layer.shape
     sig_hidden = np.c_[sig_hidden, np.ones(sig_hidden_y)]
-    
+#    
     output = np.dot(sig_hidden,w2)
-    print ("this is the output")
-    print (output)
-    print (" ")
+#    print ("this is the output")
+#    print (output)
+#    print (" ")
     sig_output = sigmoid(output)
-    print ("this is the sig output")
-    print (sig_output)
-    print (" ")
+#    print ("this is the sig output")
+#    print (sig_output)
+#    print (" ")
     labels = np.argmax(sig_output, axis=1)
-    print ("This is the label")
-    print (labels)
+#    print ("This is the label")
+#    print (labels)
     return sig_hidden[:,:-1], sig_output[:,:] , labels
 
 
@@ -263,7 +322,7 @@ train_data, train_label, validation_data, validation_label, test_data, test_labe
 n_input = train_data.shape[1]
 
 # set the number of nodes in hidden unit (not including bias unit)
-n_hidden = 50
+n_hidden = 10
 
 # set the number of nodes in output unit
 n_class = 10
@@ -283,9 +342,11 @@ args = (n_input, n_hidden, n_class, train_data, train_label, lambdaval)
 # Train Neural Network using fmin_cg or minimize from scipy,optimize module. Check documentation for a working example
 
 opts = {'maxiter': 50}  # Preferred value.
-
+global numIters
+numIters = 0
+print("Begun minimize: {}".format(dt.datetime.now()))
 nn_params = minimize(nnObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
-
+print("End minimize: {}".format(dt.datetime.now()))
 # In Case you want to use fmin_cg, you may have to split the nnObjectFunction to two functions nnObjFunctionVal
 # and nnObjGradient. Check documentation for this function before you proceed.
 # nn_params, cost = fmin_cg(nnObjFunctionVal, initialWeights, nnObjGradient,args = args, maxiter = 50)
@@ -298,6 +359,7 @@ w2 = nn_params.x[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
 # Test the computed parameters
 
 predicted_label = nnPredict(w1, w2, train_data)
+sig_h , sig_o , labelsss = nnFeedForward(w1, w2, train_data)
 
 # find the accuracy on Training Dataset
 
