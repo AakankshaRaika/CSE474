d0efae01759192786b2784ab321e7b369d58b322
DominiqueHickson <dnhickso@buffalo.edu>
2017-04-16 16:40:16 -0400

    Update script.py

--- script.py
+++ script.py
@@ -198,8 +198,8 @@ def regressionObjVal(w, X, y, lambd):
     w = np.mat(w).T
     
     error = (((y - X.dot(w)).T).dot((y - X.dot(w))) / (2*N)) + ((lambd * ((w.T).dot(w))) / 2)
-    error_grad = (((((w.T).dot((X.T).dot(X))) - ((y.T).dot(X))) / N) + ((w.T) * lambd)).T
-    #error_grad = (((-1((w.T).dot((X.T).dot(X))) + ((y.T).dot(X))) / N) + ((w.T) * lambd)).T
+    #error_grad = (((((w.T).dot((X.T).dot(X))) - ((y.T).dot(X))) / N) + ((w.T) * lambd)).T
+    error_grad = (((-1((w.T).dot((X.T).dot(X))) + ((y.T).dot(X))) / N) + ((w.T) * lambd)).T
     error_grad = np.ndarray.flatten(np.array(error_grad))
     return error, error_grad
 
